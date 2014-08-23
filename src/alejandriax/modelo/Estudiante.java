e86ccdaa87d127860f9fca34502f69d50550bedd
jmora <jmora@jmora-Satellite-C655>
2014-08-23 12:01:16 -0600

    - Creacion clase Estudiante.

---
+++ src/alejandriax/modelo/Estudiante.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package alejandriax.modelo;
+
+/**
+ * @author jmora
+ *
+ */
+public class Estudiante extends Persona {
+
+	/**
+	 * @param numeroCedula
+	 * @param nombre
+	 * @param primerApellido
+	 * @param segundoApellido
+	 */
+	public Estudiante(String numeroCedula, String nombre,
+			String primerApellido, String segundoApellido) {
+		super(numeroCedula, nombre, primerApellido, segundoApellido);
+	}
+
+}
