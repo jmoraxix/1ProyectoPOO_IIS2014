81acf5872afa390b48e2fe9d51d67b2397e014a5
rgvillalobosr <otakuroger@hotmail.com>
2014-09-13 16:09:58 -0600

    - Se agregan las imagenes de las portadas de pruebas.

---
+++ src/alejandriax/vista/imagenes/portadas/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Primer Proyecto POO
+ * Jose David Mora Loria
+ * 2014004856
+ * Diego Delgado Cerdas
+ * 2013099268
+ * David Diaz
+ * 2014004725
+ * Roger Villalobos
+ * 2014079369
+ * 13/9/2014
+ */
+/**
+ * @author Róger
+ *
+ */
+package alejandriax.vista.imagenes.portadas;
+
+/*
+ * Contenido del paquete: Contine las imagenes de las portadas usadas en las muestras.
+ * 
+ */
