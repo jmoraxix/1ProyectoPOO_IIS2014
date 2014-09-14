e3136285cf5b35c4f426c61e8610b32ce174a621
jmora <jmora@jmora-Satellite-C655>
2014-09-14 04:19:09 -0600

    - Arreglo ventana MostrarLibro.

--- src/alejandriax/vista/cliente/MostrarLibro.java
+++ src/alejandriax/vista/cliente/MostrarLibro.java
@@ -14,10 +14,12 @@ package alejandriax.vista.cliente;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import alejandriax.control.Coordinador;
+import alejandriax.modelo.Articulo;
 import alejandriax.modelo.Libro;
+import alejandriax.vista.PanelCalificacion;
+import alejandriax.vista.PanelConFondo;
 import alejandriax.vista.VentanaEmergente;
 
 /**
@@ -32,35 +34,85 @@ public class MostrarLibro extends VentanaEmergente {
 	 * @param frame
 	 * @param title
 	 */
-	public MostrarLibro(JFrame frame, String title, Libro libro) {
-		super(frame, title, "fondo_emergente.png");
-		this.libro = libro;
+	public MostrarLibro(JFrame frame, Articulo libro) {
+		super(frame, libro.getTituloArticulo(), "fondo_emergente.png");
+		this.libro = (Libro) libro;
 		inicializarVentana();
-
 	}
 
 	private void inicializarVentana() {
 		getContentPane().setLayout(null);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(55, 58, 185, 204);
-		getContentPane().add(panel);
+		PanelConFondo imagen = new PanelConFondo(libro.getImagen());
+		imagen.setBounds(81, 58, 184, 256);
+		getContentPane().add(imagen);
 
 		JLabel lblTtulo = new JLabel("T\u00EDtulo:");
-		lblTtulo.setBounds(349, 58, 45, 27);
+		lblTtulo.setBounds(347, 58, 45, 27);
 		getContentPane().add(lblTtulo);
 
 		JLabel titulo = new JLabel("");
-		titulo.setBounds(349, 110, 174, 27);
+		titulo.setText(libro.getTituloArticulo());
+		titulo.setBounds(347, 86, 174, 27);
 		getContentPane().add(titulo);
 		
 		JLabel lblCodigo = new JLabel("Código:");
-		lblCodigo.setBounds(55, 269, 79, 27);
+		lblCodigo.setBounds(55, 368, 79, 27);
 		getContentPane().add(lblCodigo);
 		
 		JLabel codigo = new JLabel("");
-		codigo.setBounds(55, 297, 115, 27);
+		codigo.setText(libro.getIdArticulo());
+		codigo.setBounds(125, 368, 115, 27);
 		getContentPane().add(codigo);
+		
+		JLabel lblAutor = new JLabel("Autor:");
+		lblAutor.setBounds(347, 125, 45, 27);
+		getContentPane().add(lblAutor);
+		
+		JLabel autor = new JLabel("");
+		autor.setText(libro.getAutor());
+		autor.setBounds(347, 153, 174, 27);
+		getContentPane().add(autor);
+		
+		JLabel lblEditorial = new JLabel("Editorial:");
+		lblEditorial.setBounds(347, 259, 92, 27);
+		getContentPane().add(lblEditorial);
+		
+		JLabel editorial = new JLabel("");
+		editorial.setText(libro.getEditorial());
+		editorial.setBounds(347, 287, 174, 27);
+		getContentPane().add(editorial);
+		
+		JLabel genero = new JLabel("");
+		genero.setText(libro.getGenero());
+		genero.setBounds(347, 220, 174, 27);
+		getContentPane().add(genero);
+		
+		JLabel lblGenero = new JLabel("Género:");
+		lblGenero.setBounds(347, 192, 73, 27);
+		getContentPane().add(lblGenero);
+		
+		JLabel lblIdioma = new JLabel("Idioma:");
+		lblIdioma.setBounds(347, 393, 92, 27);
+		getContentPane().add(lblIdioma);
+		
+		JLabel idioma = new JLabel("");
+		idioma.setText(libro.getIdioma());
+		idioma.setBounds(347, 421, 174, 27);
+		getContentPane().add(idioma);
+		
+		JLabel edicion = new JLabel("");
+		edicion.setText(libro.getEdicion());
+		edicion.setBounds(347, 354, 174, 27);
+		getContentPane().add(edicion);
+		
+		JLabel lblEdicion = new JLabel("Edición:");
+		lblEdicion.setBounds(347, 326, 92, 27);
+		getContentPane().add(lblEdicion);
+		
+		PanelCalificacion panelCalificacion = new PanelCalificacion(libro);
+		panelCalificacion.setBounds(81, 328, 184, 28);
+		getContentPane().add(panelCalificacion);
 	}
 
 	public Coordinador getCoodinador() {
